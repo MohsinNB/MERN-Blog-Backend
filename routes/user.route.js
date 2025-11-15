0c2952bea0cd2a3c9fccaf9950898b2c61329c93
Mohammad Mohsin <mohammadmohsin28012005@gmail.com>
2025-11-15 18:16:26 +0600

    add update route

--- routes/user.route.js
+++ routes/user.route.js
@@ -3,12 +3,15 @@ import {
   loginUser,
   logOut,
   registerUser,
+  updateProfile,
 } from "../controllers/user.controller.js";
+import { isAuthenticated } from "../middleware/isAuthenticated.js";
+import { singleUpload } from "../middleware/multer.js";
 
 const userroutes = Router();
 
 userroutes.post("/register", registerUser);
 userroutes.post("/login", loginUser);
 userroutes.get("/logout", logOut);
-
+userroutes.put("/profile/update", isAuthenticated, singleUpload, updateProfile);
 export default userroutes;
