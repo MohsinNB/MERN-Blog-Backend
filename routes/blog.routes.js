5e191527052b7a7e1dd6a53b3b0d10fe903e8d6c
Mohammad Mohsin <mohammadmohsin28012005@gmail.com>
2025-11-19 00:09:25 +0600

    Update blog successfully

--- routes/blog.routes.js
+++ routes/blog.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import {} from "../controllers/user.controller.js";
 import { isAuthenticated } from "../middleware/isAuthenticated.js";
-
-import { createBlog } from "../controllers/blog.controller.js";
+import { singleUpload } from "../middleware/multer.js";
+import { createBlog, updateBlog } from "../controllers/blog.controller.js";
 
 const blogRoutes = Router();
 
 blogRoutes.post("/", isAuthenticated, createBlog);
+blogRoutes.put("/:blogId", isAuthenticated, singleUpload, updateBlog);
 export default blogRoutes;
